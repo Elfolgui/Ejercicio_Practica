049ee2f194355b4c4d87974e68f3b8843b132c81
Elfolgui <aguante.los.rulos@gmail.com>
2017-11-09 20:34:09 -0300

    Empezando a verse bonito

--- Clases/Controlador.py
+++ Clases/Controlador.py
@@ -4,18 +4,18 @@ import os
 class Controlador(object):
 
     @classmethod
-    def Menu_Agregar_Barrio(cls, Ciu):
+    def Menu_Agregar_Barrio(cls, Ciu, prov):
         os.system("cls")
-        print("Ingrese un Nombre")
+        print("Ingrese un Nombre para su Barrio")
         n = input()
         os.system("cls")
-        print("Ingrese un codigo")
+        print("Un codigo para el Barrio")
         c = input()
         os.system("cls")
-        print("Ingrese Primer Punto")
+        print("Primer Par de coordenadas")
         cor_1 = input()
         os.system("cls")
-        print("Ingrese Segundo Punto")
+        print("Segundo Par de coordenadas")
         cor_2 = input()
         os.system("cls")
         print("Ingrese la cantidad de poblacion")
@@ -24,13 +24,18 @@ class Controlador(object):
         print("Ingrese la Ciudad a la que pertenece")
         pertenece = input()
         os.system("cls")
-        respuesta = Controlador.Agregar_Barrio(n, c, (cor_1, cor_2), p, pertenece)
+        respuesta = Controlador.Agregar_Barrio(n, c, (cor_1, cor_2), p, pertenece, prov)
         if respuesta is not False:
+            print("Entre")
             Ciu.Lista_Barrios.append(respuesta)
+        if respuesta is False:
+            print("Agregue una Ciudad con ese nombre antes de poder agregar un Barrio")
+            input()
+            return True
 
     @classmethod
-    def Agregar_Barrio(cls, nombre, codigo, coordenadas, poblacion, pertenece):
-        for city in Provincia.Lista_Ciudades:
+    def Agregar_Barrio(cls, nombre, codigo, coordenadas, poblacion, pertenece, prov):
+        for city in prov.Lista_Ciudades:
             if city.Nombre == pertenece:
                 barrio = Barrio(nombre, codigo, coordenadas, poblacion, pertenece)
                 return barrio
@@ -39,8 +44,8 @@ class Controlador(object):
         return False
 
     @classmethod
-    def Agregar_Ciudad(cls, nombre, codigo, coordenadas, pertenece):
-        for prov in Pais.Lista_Provincias:
+    def Agregar_Ciudad(cls, nombre, codigo, coordenadas, pertenece, pais):
+        for prov in pais.Lista_Provincias:
             if prov.Nombre == pertenece:
                 ciudad = Ciudad(nombre, codigo, coordenadas, pertenece)
                 return ciudad
@@ -49,8 +54,8 @@ class Controlador(object):
         return False
 
     @classmethod
-    def Agregar_Provincia(cls, nombre, codigo, coordenadas, pertenece):
-        for pais in Continente.Lista_Paises:
+    def Agregar_Provincia(cls, nombre, codigo, coordenadas, pertenece, con):
+        for pais in con.Lista_Paises:
             if pais.Nombre == pertenece:
                 provincia = Provincia(nombre, codigo, coordenadas, pertenece)
                 return provincia
@@ -59,8 +64,8 @@ class Controlador(object):
         return False
 
     @classmethod
-    def Agregar_Pais(cls, nombre, codigo, coordenadas, pertenece):
-        for continente in Mundo.Lista_Continentes:
+    def Agregar_Pais(cls, nombre, codigo, coordenadas, pertenece, mundo):
+        for continente in mundo.Lista_Continentes:
             if continente.Nombre == pertenece:
                 pais = Pais(nombre, codigo, coordenadas, pertenece)
                 return pais
@@ -69,11 +74,24 @@ class Controlador(object):
         return False
 
     @classmethod
-    def Agregar_Continente(cls, nombre, codigo, coordenadas):
-        for continente in Mundo.Lista_Continentes:
+    def Agregar_Continente(cls, nombre, codigo, coordenadas, mundo):
+        for continente in mundo.Lista_Continentes:
             if continente.Nombre != nombre:
                 pass
             else:
                 return False
         continente = Continente(nombre, codigo, coordenadas)
         return continente
+
+    @classmethod
+    def Mostrar_Barrios(cls, mundo):
+        os.system("cls")
+        for con in mundo.Lista_Continentes:
+            for pa in con.Lista_Paises:
+                for pro in pa.Lista_Provincias:
+                    for ciu in pro.Lista_Ciudades:
+                        print("|" + ciu.Nombre + "|" + "\n")
+                        for bar in ciu.Lista_Barrios:
+                            print("|" + bar.Nombre + "|")
+        print("\n" + "Presione enter para volver")
+        input()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Clases import *
 import os
 
-Con = Continente("Con", 747, ((0, 0),(100, 100)))
+Con = Continente("Con", 747, ((0, 0), (100, 100)))
 
 Bar = Barrio("Bar", 202, ((20, 20), (30, 30)), 30000, "Ciu")
 
@@ -11,17 +11,34 @@ Prov = Provincia("Prov", 654, ((40, 40), (50, 50)), "Pa")
 
 Pa = Pais("Pa", 951, ((50, 50),(60, 60)), "Con")
 
+Mundi = Mundo()
+
+Ciu.Lista_Barrios.append(Bar)
+Prov.Lista_Ciudades.append(Ciu)
+Pa.Lista_Provincias.append(Prov)
+Con.Lista_Paises.append(Pa)
+Mundi.Lista_Continentes.append(Con)
+
+
 while True:
+    os.system("cls")
     print("1: Agregar Barrio")
     print("2: Agregar Ciudad")
     print("3: Agregar Provincias")
     print("4: Agregar Pais")
     print("5: Agregar Continente")
+    print("6: Mostrar Barrios")
+    print("7: Mostrar Ciudades")
+    print("8: Mostrar Provincias")
+    print("9: Mostrar Paises")
+    print("10: Mostrar Continentes")
+    print("0: Salir")
 
     selector = input()
 
     if selector == "1":
-        Controlador.Menu_Agregar_Barrio(Ciu)
+        if Controlador.Menu_Agregar_Barrio(Ciu, Prov):
+            pass
     if selector == "2":
         Controlador.Agregar_Ciudad()
     if selector == "3":
@@ -30,3 +47,7 @@ while True:
         Controlador.Agregar_Pais()
     if selector == "5":
         Controlador.Agregar_Continente()
+    if selector == "6":
+        Controlador.Mostrar_Barrios(Mundi)
+    if selector == "0":
+        break
